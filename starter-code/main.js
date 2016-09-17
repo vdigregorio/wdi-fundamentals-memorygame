689570fe93b870d2b7f48d9401bb9958347d7668
vdigregorio <vdigregorio@Valerias-MacBook-Air.local>
2016-09-17 16:12:30 -0400

    js update
    if else statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,18 @@
-console.log
+console.log()
 
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardOne === cardThree) {
+    alert("You have a match!");
+} else {
+    alert("Sorry, try again.");
+}
+
+if (cardTwo === cardFour) {
+    alert("You have a match!");
+} else {
+    alert("Sorry, try again.");
+}
